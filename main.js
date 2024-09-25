a54a27201a8de69a787da9f2482020f9191f0e17
Cody <wilcoyonkin@gmail.com>
2024-09-25 10:01:16 -0400

    working on transitioning to mongodb

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
-function toggleSidebar () {
-    let sidebar = document.getElementById("sidebar");
-    if(sidebar.style.width === "18rem") {
-        sidebar.style.width = "0";
-    } else {
-        sidebar.style.width = "18rem";
-    }
-}   
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://wilcoyonkin:LHdK7vEZTBYNwzo8@scholarbase.nfxwu.mongodb.net/?retryWrites=true&w=majority&appName=scholarBase";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
