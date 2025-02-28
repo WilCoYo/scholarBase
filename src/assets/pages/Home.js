31799740e46ece8f635f33fb956c07f26efb74e0
W. Cody Yonkin <WilCoYonkin@gmail.com>
2025-02-27 20:47:50 -0500

    added server.js file that configured backend with express

--- src/assets/pages/Home.js
+++ src/assets/pages/Home.js
@@ -1,14 +1,42 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './Home.css'
 
+
+
 function Home() {
+
+const [searchTerm, setSearchTerm ] = useState('')
+
+const [searchResults, setSearchResults] = useState('');
+
+
+
+
+const handleSearch = () => {
+    setSearchTerm(searchTerm)
+}
+
+useEffect(() => {
+    const results = db.collection('articles').find({searchTerm})
+    setSearchResults(results)
+}, [searchTerm])
+
   return (
     <div className='home-page'>
         <h1>scholar<strong>Base</strong></h1>
         <input 
             type='text'
-            placeholder='Search databse'
+            placeholder='Search database'
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
         />
+        <button
+            onClick={handleSearch}
+            className='searchBtn'
+        >
+            Search
+        </button>
+        <p>{searchResults}</p>
     </div>
   )
 }
