fce9e22ec19c2f71e49ff8f467e082aa2279e071
W. Cody Yonkin <WilCoYonkin@gmail.com>
2025-03-01 14:13:14 -0500

    trying to fix deploy issues

--- next.config.js
+++ next.config.js
@@ -1,22 +1,8 @@
-module.exports = {
-    async rewrites() {
-      return [
-        {
-          source: '/about',
-          destination: '/',
-        },
-      ]
-    },
+// @ts-check
+ 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    /* config options here */
   }
-
-  module.exports = {
-    async redirects() {
-      return [
-        {
-          source: '/about',
-          destination: '/',
-          permanent: true,
-        },
-      ]
-    },
-  }
+   
+  module.exports = nextConfig
