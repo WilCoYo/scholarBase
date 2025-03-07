4feb2474af3042e8cc95857e612a1b1c4e08d638
W. Cody Yonkin <WilCoYonkin@gmail.com>
2025-03-07 13:15:03 -0500

    added functionality to clicking 'scholarBase' H1 do it resets results container

--- src/assets/pages/Home.js
+++ src/assets/pages/Home.js
@@ -12,7 +12,6 @@ const [isLoading, setIsLoading] = useState(false);
 const [error, setError] = useState(null);
 
 
-
 const handleSearch = async () => {
     if(!searchTerm.trim()) return
 
@@ -48,34 +47,7 @@ const handleSearch = async () => {
         setIsLoading(false);
     }
 
-  
 
-
-//     try {
-//         const apiBaseUrl = `https://${process.env.REACT_APP_API_BASE_URL}`;
-//         const searchUrl = `${apiBaseUrl}/api/search?term=${encodeURIComponent(searchTerm)}`;
-        
-//         console.log('Fetching URL:', searchUrl);  // Log the exact URL being called
-
-//         const response = await fetch(searchUrl);
-        
-//         // Log the raw response for debugging
-//         const responseText = await response.text();
-//         // console.log('Raw Response:', responseText);
-
-//         if(!response.ok) {
-//             throw new Error(`Search failed with status ${response.status}: ${responseText}`);
-//         }
-
-//         // Parse the JSON manually
-//         const data = JSON.parse(responseText);
-//         setSearchResults(data)
-//     } catch (err) {
-//         setError(`An error occurred while searching: ${err.message}`);
-//         console.error('Search error:', err);
-//     } finally {
-//         setIsLoading(false);
-//     }
 }
 
 useEffect(() => {
@@ -92,10 +64,20 @@ useEffect(() => {
     ));
   };
 
+
+
+  const handleClick = () => {
+    setSearchTerm('');
+    setSearchResults([]);
+};
  
   return (
     <div className='home-page'>
-        <h1 className='title'>scholar<strong>Base</strong></h1>
+        <h1 
+            className='title'
+            onClick={handleClick}>
+                scholar<strong>Base</strong>
+        </h1>
         <div className='search-container'>
             <input 
                  type='text'
